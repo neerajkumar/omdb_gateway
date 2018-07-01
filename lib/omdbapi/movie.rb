1b899d1a58468bd915acac2f463a8c6dffc3a4db
Neeraj Kumar <neeraj.kumar@gmail.com>
2018-07-01 19:09:52 +0530

    correcting omdb.yml file path

--- lib/omdbapi/movie.rb
+++ lib/omdbapi/movie.rb
@@ -7,7 +7,7 @@ module Omdbapi
       raise InvalidIMDBID.new('IMDB ID not present') if imdb_id.empty? || imdb_id.nil?
 
       raise InvalidConfiguration.new('omdb.yml file not present') unless File.exists?('config/omdb.yml')
-      omdb_config = YAML::load(File.open('config/omdb.yml'))
+      omdb_config = YAML::load(File.open('config/initializers/omdb.yml'))
       @response ||= RestClient.get("#{BASE_URI}/?apikey=#{omdb_config['apikey']}&i=#{imdb_id}")
       JSON.parse(@response.body).with_indifferent_access
     end
